33e16c81dcf443b3b9d60e0e4bf11052fe983fae
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-17 16:05:13 -0300

    Deploy- bdd

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-document.querySelector(".btn_menu").addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("show");
-}
