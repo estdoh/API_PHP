973e669e8f3ff4d61df65c27e7ac220c5e510c02
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-17 14:32:23 -0300

    deploy-APIREST

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a44bb076c580d00ef1bfba04ec88caef48f7f93f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a44bb076c580d00ef1bfba04ec88caef48f7f93f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
