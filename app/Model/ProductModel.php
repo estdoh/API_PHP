2691b25e1f390518edbbd4dc5a6a52acaedbb03a
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-18 16:54:48 -0300

    bdd model

--- app/Model/ProductModel.php
+++ app/Model/ProductModel.php
@@ -17,6 +17,7 @@ class ProductsModel {
         $query->execute();
         $products = $query->fetchAll(PDO::FETCH_OBJ);
         return $products;
+        echo $products;
     }
 
     function getProductById($id) {
