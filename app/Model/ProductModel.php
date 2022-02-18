0f31190a17c68bee07fba0d78641d4520ae65444
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-18 11:27:42 -0300

    bdd model

--- app/Model/ProductModel.php
+++ app/Model/ProductModel.php
@@ -16,7 +16,7 @@ class ProductsModel {
         // $this->db = new mysqli($server,$username,$password,$db);
         // $conn = new mysqli($server, $username, $password, $db);
 
-        $this->db = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;b=heroku_3768f01bf6856fc','username=be3093d987bbca','password=14377f76', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+        $this->db = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;db=heroku_3768f01bf6856fc','username=be3093d987bbca','password=14377f76', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
         // $this->db = new PDO('mysql:host=localhost;'.'dbname=apirest_mydate;charset=utf8', 'apirest_mydate', '');        
         // $this->db = new PDO('mysql:host=localhost;'.'dbname=apirest_mydate;charset=utf8', 'apirest_usr1', 'Qe8raDs78g');
     }
