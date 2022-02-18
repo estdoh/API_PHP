1eedb53cedfe0e57341db21e62a5424338277dca
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-18 10:03:44 -0300

    bdd model

--- app/Model/ProductModel.php
+++ app/Model/ProductModel.php
@@ -9,14 +9,14 @@ class ProductsModel {
         // $this->db = new PDO('mysql:host=localhost;'.'dbname=tpe_especial;charset=utf8', 'root', '');
 
         // $url = parse_url(getenv("mysql://be3093d987bbca:14377f76@us-cdbr-east-05.cleardb.net/heroku_3768f01bf6856fc?reconnect=true"));
-        $server = 'us-cdbr-east-05.cleardb.net';
-        $username = 'be3093d987bbca';
-        $password = '14377f76';
-        $db = 'heroku_3768f01bf6856fc';
+        // $server = 'us-cdbr-east-05.cleardb.net';
+        // $username = 'be3093d987bbca';
+        // $password = '14377f76';
+        // $db = 'heroku_3768f01bf6856fc';
         // $this->db = new mysqli($server,$username,$password,$db);
-        $conn = new mysqli($server, $username, $password, $db);
+        // $conn = new mysqli($server, $username, $password, $db);
 
-        // $this->db = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;','Username=be3093d987bbca','Password=14377f76','dbname=heroku_3768f01bf6856fc;charset=utf8');
+        $this->db = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;','Username=be3093d987bbca','Password=14377f76','dbname=heroku_3768f01bf6856fc;charset=utf8');
         // $this->db = new PDO('mysql:host=localhost;'.'dbname=apirest_mydate;charset=utf8', 'apirest_mydate', '');        
         // $this->db = new PDO('mysql:host=localhost;'.'dbname=apirest_mydate;charset=utf8', 'apirest_usr1', 'Qe8raDs78g');
     }
