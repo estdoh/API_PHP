6bcfaeebf44c60d0d9d7bc4855dff3e22fedfba2
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-18 15:18:29 -0300

    bdd model

--- app/Model/ProductModel.php
+++ app/Model/ProductModel.php
@@ -13,7 +13,9 @@ class ProductsModel {
     
     function getProducts() {
         // $query = $this->db->prepare('SELECT products.*,category.name as name_category FROM products JOIN category ON products.category = category.id_category');
-        $query = $this->db->prepare("SELECT DATABASE()");
+        // select db heroku_3768f01bf6856fc
+        $query = select_db('heroku_3768f01bf6856fc');
+        // $query = select_db(heroku_3768f01bf6856fc);
          $query = $this->db->prepare("SELECT * FROM products");
         $query->execute();
         $products = $query->fetchAll(PDO::FETCH_OBJ);
