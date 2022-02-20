e874c61587277256203a8ec3c68f114bb4ed5bdf
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-20 01:16:32 -0300

    ApiHelpers

--- app/Controller/ApiUserController.php
+++ app/Controller/ApiUserController.php
@@ -37,7 +37,7 @@ class ApiUserController{
         $getUser = $this->model->getUserById($id);
         if(!empty($getUser)){
             // return $this->view->response($getUser, 200);
-            if(!null($user)){
+            if($user){
                 return $this->view->response($getUser, 200);                
             } else {
                 $this->view->response("Forbidden", 403);
