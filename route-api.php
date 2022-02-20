0efde3a5bb86c398bfbf14ea981a9a6a9af97296
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-02-20 00:18:19 -0300

    SQL consultas DEFINITIVO v1

--- route-api.php
+++ route-api.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'libs/Router.php';
 require_once 'app/Controller/ApiProductsController.php';
-require_once 'app/Controller/ApiCategoryController.php';
-require_once 'app/Controller/ApiBudgetsController.php';
-require_once 'app/Controller/ApiClientsController.php';
-require_once 'app/Controller/ApiUserController.php';
+// require_once 'app/Controller/ApiCategoryController.php';
+// require_once 'app/Controller/ApiBudgetsController.php';
+// require_once 'app/Controller/ApiClientsController.php';
+// require_once 'app/Controller/ApiUserController.php';
 // require_once 'app/Controller/ApiCommentsController.php';
 
 // crea el router
@@ -19,23 +19,23 @@ $router->addRoute('products', 'POST', 'ApiProductsController', 'insertProduct');
 $router->addRoute('products/:ID', 'PUT', 'ApiProductsController', 'editProduct');
 $router->addRoute('products/:ID', 'DELETE', 'ApiProductsController', 'deleteProduct');
 
-$router->addRoute('categories', 'GET', 'ApiCategoryController', 'getCategories');
-$router->addRoute('categories/:ID', 'GET', 'ApiCategoryController', 'getCategory');
-$router->addRoute('categories', 'POST', 'ApiCategoryController', 'insertCategory');
-$router->addRoute('categories/:ID', 'PUT', 'ApiCategoryController', 'editCategory');
-$router->addRoute('categories/:ID', 'DELETE', 'ApiCategoryController', 'deleteCategory');
+// $router->addRoute('categories', 'GET', 'ApiCategoryController', 'getCategories');
+// $router->addRoute('categories/:ID', 'GET', 'ApiCategoryController', 'getCategory');
+// $router->addRoute('categories', 'POST', 'ApiCategoryController', 'insertCategory');
+// $router->addRoute('categories/:ID', 'PUT', 'ApiCategoryController', 'editCategory');
+// $router->addRoute('categories/:ID', 'DELETE', 'ApiCategoryController', 'deleteCategory');
 
-$router->addRoute('budget', 'GET', 'ApiBudgetsController', 'getBudgets');
-$router->addRoute('budget/:ID', 'GET', 'ApiBudgetsController', 'getBudget');
-$router->addRoute('budget', 'POST', 'ApiBudgetsController', 'insertBudget');
-$router->addRoute('budget/:ID', 'PUT', 'ApiBudgetsController', 'editBudget');
-$router->addRoute('budget/:ID', 'DELETE', 'ApiBudgetsController', 'deleteBudget');
+// $router->addRoute('budget', 'GET', 'ApiBudgetsController', 'getBudgets');
+// $router->addRoute('budget/:ID', 'GET', 'ApiBudgetsController', 'getBudget');
+// $router->addRoute('budget', 'POST', 'ApiBudgetsController', 'insertBudget');
+// $router->addRoute('budget/:ID', 'PUT', 'ApiBudgetsController', 'editBudget');
+// $router->addRoute('budget/:ID', 'DELETE', 'ApiBudgetsController', 'deleteBudget');
 
-$router->addRoute('clients', 'GET', 'ApiClientsController', 'getClients');
-$router->addRoute('clients/:ID', 'GET', 'ApiClientsController', 'getClient');
-$router->addRoute('clients', 'POST', 'ApiClientsController', 'insertClient');
-$router->addRoute('clients/:ID', 'PUT', 'ApiClientsController', 'editClients');
-$router->addRoute('clients/:ID', 'DELETE', 'ApiClientsController', 'deleteClient');
+// $router->addRoute('clients', 'GET', 'ApiClientsController', 'getClients');
+// $router->addRoute('clients/:ID', 'GET', 'ApiClientsController', 'getClient');
+// $router->addRoute('clients', 'POST', 'ApiClientsController', 'insertClient');
+// $router->addRoute('clients/:ID', 'PUT', 'ApiClientsController', 'editClients');
+// $router->addRoute('clients/:ID', 'DELETE', 'ApiClientsController', 'deleteClient');
 
 // $router->addRoute('comments', 'GET', 'ApiCommentsController', 'getComments');
 // $router->addRoute('comments/products/:ID', 'GET', 'ApiCommentsController', 'getCommentsByProductId');
