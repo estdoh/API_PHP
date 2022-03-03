81519e9089069bdfa74e56005f6b4e48be496ac8
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-03-03 00:09:25 -0300

    headers edit

--- app/View/ApiView.php
+++ app/View/ApiView.php
@@ -3,7 +3,10 @@
 class ApiView{
 
     public function response($data, $status) {
-        header("Content-Type: application/json");
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header("Content-Type: application/json; charset=UTF-8");
         header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
         echo json_encode($data);
     }
